2a39898186de510668e66be8812f39023d5ac162
lqthai axon <lqthai@axonactive.com>
2022-07-27 16:27:19 +0700

    authe

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./*.{js,jsx,ts,tsx}",
+    "./components/*.{js,jsx,ts,tsx}",
+    "./screens/*.{js,jsx,ts,tsx}",
+
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+  corePlugins: require("tailwind-rn/unsupported-core-plugins"),
+};
