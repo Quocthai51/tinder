5dd1631ac063430ce4d889c3a22083f3fa3743f3
lqthai axon <lqthai@axonactive.com>
2022-07-27 17:21:21 +0700

    a

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,9 +1,15 @@
 import { View, Text } from "react-native";
 import React, { createContext, useContext } from "react";
+import * as Google from "expo-google-app-auth";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
+
+  const signInWithGoogle = async () => {
+    await Google.logInAsync({})
+  }
+
   return (
     <AuthContext.Provider value={{ user: null }}>
       {children}
