96bcc8bcb80a45550a767587fefd305a7c25732f
lqthai axon <lqthai@axonactive.com>
2022-07-27 17:42:19 +0700

    as

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -4,14 +4,26 @@ import * as Google from "expo-google-app-auth";
 
 const AuthContext = createContext({});
 
-export const AuthProvider = ({ children }) => {
+const config = {
+  androidClientId:
+    "474618568505-okgvfukopl3b8pi5dhvara387et193da.apps.googleusercontent.com",
+  iosClientId:
+    "474618568505-bn1uokeh8nrbodmgjplfmnhaogrkbbgh.apps.googleusercontent.com",
+  scopes: ["profile", "email"],
+  permissions: ["public_profile", "email", "gender", "location"],
+};
 
+export const AuthProvider = ({ children }) => {
   const signInWithGoogle = async () => {
-    await Google.logInAsync({})
-  }
+    Google.logInAsync(config).then(async (logInResult) => {
+      if (logInResult.type === "success") {
+        //login..
+      }
+    });
+  };
 
   return (
-    <AuthContext.Provider value={{ user: null }}>
+    <AuthContext.Provider value={{ user: null, signInWithGoogle }}>
       {children}
     </AuthContext.Provider>
   );
